625368c2516a1e9f94bed6d41ae607bf0f7cb568
Mustapha ALIBOURKI <64364329+mustaphalib@users.noreply.github.com>
2022-07-08 18:40:20 +0200

    addding new person for testing with webhook

--- khalednoha-maven-java-example-master/src/main/java/com/example/service/PersonService.java
+++ khalednoha-maven-java-example-master/src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,15 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("5");
+		p.setAge(27);
+		p.setFirstName("ALIBOURLI");
+		p.setLastName("MUSTAPHA");
+		persons.add(p);
+		
+		
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
